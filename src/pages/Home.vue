66893d1dd93fa2a41502bf88b21aa7962709f52d
shakerhp <99677349+shakerhp@users.noreply.github.com>
2022-10-08 12:02:05 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Navbar />
+    <section class="home">
+      <div class="image">
+        <img src="https://media.discordapp.net/attachments/887300703314387035/1018101275918860338/106097658_n-modified.png?width=576&height=576" alt="" />
+      </div>
+      <div class="content">
+        <h3>hi, i am {{name}}</h3>
+        <span>Belive the Impossible is Possible.</span>
+        <p>
+          {{motto}}
+        </p>
+        <p>{{content}}</p>
+        <router-link to="/about" class="btn">
+          about me <i class="fas fa-user"></i>
+        </router-link>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: { Navbar },
+  data() {
+    return {
+      name: "Nikorn Nuanaun",
+      motto:"GGEZ",
+      content:"GGWP",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/Projects.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <Navbar />
+        <section class="projects">
+            <h1 class="heading"> <span>my</span> projects </h1>
+            <h3>{{project_name}}</h3>
+            <p>{{project_detail}}</p>
+            <div class="box-container">
+                <iframe width="720" height="405" src="https://www.youtube.com/embed/603Hc7ycIS0"
+                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+            gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar },
+    data() {
+    return {
+      project_name: "Electrical appliances in smart hotel by web application",
+      project_detail:"การออกแบบและสร้างอุปกรณ์เครื่องมือควบคุมเครื่องใช้ไฟฟ้าในแต่ละส่วนของห้องโรงแรมโดยสามารถควบคุมได้จากภายในห้องและจากส่วนกลาง",
+    };
+  },
+};
+
+</script>
+
+<style>
+
+</style>
